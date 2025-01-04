6595a1bb2a8851934f719dead9071dd2eda503c1
KEVINPATEL006 <kevinrangpadiya805@gmail.com>
2025-01-04 18:39:53 +0530

    Update ANDgate.vhd

--- AND_gate_dataflow/ANDgate.vhd
+++ AND_gate_dataflow/ANDgate.vhd
@@ -1,14 +1,15 @@
-
+---- include std_logic_1164 library----
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-
+----Define input and output for this circuit----
 entity ANDgate is
     Port ( A : in  STD_LOGIC;
            B : in  STD_LOGIC;
            c : out  STD_LOGIC);
 end ANDgate;
 
+----write main logic here---------
 architecture Behavioral of ANDgate is
 
 begin
